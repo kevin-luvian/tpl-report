c27f904a44ecb3744f0cd22cc057b54adde3ccf1
kevin-luvian <kevinlh90@gmail.com>
2020-10-03 20:34:33 +0700

    feat: app 3 - asynchronous function

---
+++ app3_async/app/src/main/java/com/example/app3_async/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.app3_async;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int backCounter = 0;
+    private int asyncCounter = 0;
+    private Handler handler;
+    private TextView textCounter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        handler = new Handler();
+        textCounter = findViewById(R.id.textCounter);
+
+        Runnable counterRunner = new Runnable() {
+            @Override
+            public void run() {
+                textCounter.setText(getResources().getString(R.string.counter, asyncCounter++));
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.post(counterRunner);
+
+        Button button = findViewById(R.id.buttonExit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        TextView textMain = findViewById(R.id.textMain);
+        textMain.setText(getResources().getString(R.string.onBack, backCounter++));
+        // moveTaskToBack(true);
+    }
+}
